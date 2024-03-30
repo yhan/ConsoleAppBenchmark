4f1c3cfde0326196b553991e6f711e333262101e
yhan <hanyi8110@gmail.com>
2024-03-30 22:37:40 +0100

    init benchmark non virtual

---
+++ NoVirtual.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleAppBenchmark;
+
+
+public abstract class NoVirtual
+{
+    public int Offset { get; }
+    protected NoVirtual(int offset)
+    {
+        Offset = offset;
+    }
+}
+
+public class NoVirtualImpl1 : NoVirtual
+{
+    public NoVirtualImpl1() : base(1) {}
+    public int Hello() => 42;
+}
+
+
+public class NoVirtualImpl2 : NoVirtual
+{
+    public NoVirtualImpl2() : base(2) {}
+    public int Hello() => 1;
+}
+
+public abstract class WithVirtual
+{
+    public abstract int Hello();
+}
+
+class WithVirtualImpl1 : WithVirtual
+{
+    public override int Hello()
+    {
+        return 42;
+    }
+}
+
+class WithVirtualImpl2 : WithVirtual
+{
+    public override int Hello()
+    {
+        return 1;
+    }
+}
